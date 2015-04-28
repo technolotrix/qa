3de33a573af04049675640c445f40af3b97e9533
technolotrix <technolotrix@gmail.com>
2015-04-28 01:11:52 -0400

    tests for resumator page

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -5,8 +5,8 @@ import nose
 class TestApi():
 
     def setUp(self):
-        self.baseurl = 'http://nicolesmith.nyc'
-        #self.baseurl = 'http://127.0.0.1:4747'
+        #self.baseurl = 'http://nicolesmith.nyc'
+        self.baseurl = 'http://127.0.0.1:4747'
         
     def tearDown(self):
         pass
@@ -15,84 +15,95 @@ class TestApi():
 
         url = self.baseurl + '/'
 
-        for n in range(1000):
-            # GET
-            get = requests.get(url)
-            assert get.status_code == 200
-            # POST
-            post = requests.post(url, data={})
-            assert post.status_code == 405
-            # PUT
-            put = requests.put(url, data={})
-            assert put.status_code == 405
-            # DELETE
-            delete = requests.delete(url, data={})
-            assert delete.status_code == 405
+        # GET
+        get = requests.get(url)
+        assert get.status_code == 200
+        # POST
+        post = requests.post(url, data={})
+        assert post.status_code == 405
+        # PUT
+        put = requests.put(url, data={})
+        assert put.status_code == 405
+        # DELETE
+        delete = requests.delete(url, data={})
+        assert delete.status_code == 405
 
     def test_about(self):
 
         url = self.baseurl + '/about'
         # GET
-        for n in range(1000):
-            get = requests.get(url)
-            assert get.status_code == 200
-            # POST
-            post = requests.post(url, data={})
-            assert post.status_code == 405
-            # PUT
-            put = requests.put(url, data={})
-            assert put.status_code == 405
-            # DELETE
-            delete = requests.delete(url, data={})
-            assert delete.status_code == 405
+        get = requests.get(url)
+        assert get.status_code == 200
+        # POST
+        post = requests.post(url, data={})
+        assert post.status_code == 405
+        # PUT
+        put = requests.put(url, data={})
+        assert put.status_code == 405
+        # DELETE
+        delete = requests.delete(url, data={})
+        assert delete.status_code == 405
+
+    def test_resumator(self):
+
+        url = self.baseurl + '/resumator'
+        # GET
+        get = requests.get(url)
+        assert get.status_code == 200
+        # POST
+        post = requests.post(url, data={})
+        assert post.status_code == 405
+        # PUT
+        put = requests.put(url, data={})
+        assert put.status_code == 405
+        # DELETE
+        delete = requests.delete(url, data={})
+        assert delete.status_code == 405
 
     def test_projects(self):
 
         url = self.baseurl + '/projects'
         # GET
-        for n in range(1000):
-            get = requests.get(url)
-            assert get.status_code == 200
-            # POST
-            post = requests.post(url, data={})
-            assert post.status_code == 405
-            # PUT
-            put = requests.put(url, data={})
-            assert put.status_code == 405
-            # DELETE
-            delete = requests.delete(url, data={})
-            assert delete.status_code == 405
+        get = requests.get(url)
+        assert get.status_code == 200
+        # POST
+        post = requests.post(url, data={})
+        assert post.status_code == 405
+        # PUT
+        put = requests.put(url, data={})
+        assert put.status_code == 405
+        # DELETE
+        delete = requests.delete(url, data={})
+        assert delete.status_code == 405
 
     def test_services(self):
 
         url = self.baseurl + '/services'
         # GET
-        for n in range(1000):
-            get = requests.get(url)
-            assert get.status_code == 200
-            # POST
-            post = requests.post(url, data={})
-            assert post.status_code == 405
-            # PUT
-            put = requests.put(url, data={})
-            assert put.status_code == 405
-            # DELETE
-            delete = requests.delete(url, data={})
-            assert delete.status_code == 405
+        get = requests.get(url)
+        assert get.status_code == 200
+        # POST
+        post = requests.post(url, data={})
+        assert post.status_code == 405
+        # PUT
+        put = requests.put(url, data={})
+        assert put.status_code == 405
+        # DELETE
+        delete = requests.delete(url, data={})
+        assert delete.status_code == 405
 
     def test_contact(self):
 
         url = self.baseurl + '/contact'
         # GET
-        for n in range(1000):
-            get = requests.get(url)
-            assert get.status_code == 200
-            # POST
-            post = requests.post(url, data={})
-            assert post.status_code == 405
-            # PUT
-            put = requests.put(url, data={})
-            assert put.status_code == 405
-            # DELETE
-            delete = requests.delete(url, data={})
-            assert delete.status_code == 405
+        get = requests.get(url)
+        assert get.status_code == 200
+        # POST
+        post = requests.post(url, data={})
+        assert post.status_code == 405
+        # PUT
+        put = requests.put(url, data={})
+        assert put.status_code == 405
+        # DELETE
+        delete = requests.delete(url, data={})
+        assert delete.status_code == 405
